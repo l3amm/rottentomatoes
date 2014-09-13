040475bf0424e853c88c705c72724499d4a3115f
Scott Woody <swoody@dropbox.com>
2014-09-13 09:38:02 -0700

    Initial Commit

---
+++ Rotten tomatoes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rotten tomatoes
+//
+//  Created by Scott Woody on 9/13/14.
+//  Copyright (c) 2014 Scott Woody. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
